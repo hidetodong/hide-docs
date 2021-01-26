2784b15f3fcd612540774c09a1c61674c77907c1
Hidetodong <121945480@qq.com>
2021-01-26 10:03:39 +0800

    上传测试

--- devScripts/deploy.sh
+++ devScripts/deploy.sh
@@ -7,7 +7,7 @@ git add . && git commit -m $1 && git push
 REMOTE_DIR=/var/www/html/site/hide-docs/hide-docs
 REMOTE_IP=47.111.154.112
 REMOTE_USER=root
-password=512303dj
+password=512303dj#
 echo "[----- 开始执行自动上传 -----]"
 expect -c "
     spawn ssh -p22 ${REMOTE_USER}@${REMOTE_IP}
