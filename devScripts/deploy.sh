d8be9354b46f6b6ca4ac3b6db9c46c4c14c5fe83
Hidetodong <121945480@qq.com>
2021-01-26 10:03:13 +0800

    上传测试

---
+++ devScripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+git pull
+git add . && git commit -m $1 && git push
+
+# PROJECT_DIR=~/Documents/WorkCode.nosync/galaxy-1.0/galaxy/dist/web
+REMOTE_DIR=/var/www/html/site/hide-docs/hide-docs
+REMOTE_IP=47.111.154.112
+REMOTE_USER=root
+password=512303dj
+echo "[----- 开始执行自动上传 -----]"
+expect -c "
+    spawn ssh -p22 ${REMOTE_USER}@${REMOTE_IP}
+    expect \"password:\"
+    send \"${password}\r\" 
+    expect eof
+"
+echo "[----- 上传完成 -----]"
